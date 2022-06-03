920d29ff4fe3aa2c7e2f483bffc031385dd7051e
SalvoScavo <101093780+SalvoScavo@users.noreply.github.com>
2022-06-03 16:53:27 +0200

    main completo

--- Alberi/main.cpp
+++ Alberi/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-#include "BST_H"
+#include "BST_H.h"
 using namespace std;
 int main()
 {
-    BST tree;
+    BST<int> tree;
 
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(2);
+    tree.insert(4);
     tree.insert(10);
+    tree.insert(6);
+
+    tree.print();
 }

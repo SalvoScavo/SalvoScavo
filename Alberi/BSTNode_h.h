f64cc7f08268fc624614493a086c1dbab4e60513
SalvoScavo <101093780+SalvoScavo@users.noreply.github.com>
2022-06-06 11:02:17 +0200

    Successore, precedente, Max,Min

--- Alberi/BSTNode_h.h
+++ Alberi/BSTNode_h.h
@@ -15,6 +15,7 @@ class BSTNode
              T key;
              BSTNode<T>* left;
              BSTNode<T>* right;
+             BSTNode<T>* parent;
              template <typename U>   //siccome ancora lista non esiste non sa che List sarà una classe template
              friend class BST;   //ci sarà una clsse list (ancora non definito) che sarà friend di Node  
     public: 
@@ -22,6 +23,18 @@ class BSTNode
             {
                 left= nullptr;
                 right= nullptr;
+                parent= nullptr;
+            }
+
+
+            void setParent(BSTNode<T>* par)
+            {
+                this->parent=par;
+            }
+
+            BSTNode<T>* getParent() const
+            {
+                return this->parent;
             }
 
             void setLeft(BSTNode<T>* lf)
@@ -57,7 +70,7 @@ class BSTNode
 
             friend ostream& operator<<(ostream& out, const BSTNode<T> & node)
             {
-                out<<"BSTNode@"<<&node<<" key="<<(node.key) <<"- left= "<<node.left<<" - right= "<<node.right<<endl;
+                out<<"BSTNode@"<<&node<<" key="<<(node.key)<<"- parent= "<<node.parent <<"- left= "<<node.left<<" - right= "<<node.right<<endl;
                 return out;
             }
 
